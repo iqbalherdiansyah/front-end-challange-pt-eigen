58ffe7e6c243730dadd6462d5608786824e7dfcb
iqbalherdiansyah <iqbalherdiansyah204@gmail.com>
2022-10-20 00:15:30 +0700

    mengubah route

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import { Layout } from 'antd';
 import Home from './Home/Home';
 
 function App() {
-  const { Header, Content } = Layout;
+  const { Content } = Layout;
 
   return (
     <Layout style={{ backgroundColor : 'var(--black)' }}>
       <Content>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="*" element={<p>Path not resolved</p>} />
         </Routes>
       </Content>
