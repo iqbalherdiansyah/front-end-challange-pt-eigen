c299744a1dde9266468e073a88dc8c9ccf6b1c40
iqbalherdiansyah <iqbalherdiansyah204@gmail.com>
2022-10-20 00:07:30 +0700

    mengubah file app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
       <Content>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path="*" element={<p>Path not resolved</p>} />
         </Routes>
       </Content>
     </Layout>
